52eea9f52ba62dd26ac3f556215c601a03202253
Bill Chan <heiheychan@MacBook-Pro.local>
2023-08-04 00:05:44 -0400

    Implemented search query and record filters

--- app/api/record/route.js
+++ app/api/record/route.js
@@ -1,8 +1,33 @@
 import { prisma } from "@/lib/db";
 
 export async function GET(request) {
+  console.log(request.nextUrl.searchParams)
+  const property =
+  request.nextUrl.searchParams.get("property") === null
+    ? []
+    : request.nextUrl.searchParams.get("property").split("_").map(ele => +ele);
+const startdate =
+  request.nextUrl.searchParams.get("startdate") === null
+    ? new Date()
+    : new Date(request.nextUrl.searchParams.get("startdate"));
+const enddate =
+  request.nextUrl.searchParams.get("enddate") === null
+    ? new Date()
+    : new Date(request.nextUrl.searchParams.get("enddate"));
+
+    console.log(enddate, startdate)
+
   try {
     const response = await prisma.record.findMany({
+      where: {
+        propertyId: {
+          in: property
+        },
+        happened_on: {
+          lte: enddate,
+          gte: startdate,
+        }
+      },
       include: {
         property: {
           select: {
@@ -16,6 +41,7 @@ export async function GET(request) {
     });
     return new Response(JSON.stringify({ response }), { status: 200 });
   } catch (e) {
+    console.log(e)
     return new Response(JSON.stringify({ message: "fail" }), { status: 400 });
   }
 }
--- app/page.js
+++ app/page.js
@@ -3,17 +3,18 @@
 import "@wojtekmaj/react-daterange-picker/dist/DateRangePicker.css";
 import "react-calendar/dist/Calendar.css";
 
-import { useEffect, useState } from "react";
+import axios from "axios";
+import { useEffect, useState, useMemo } from "react";
 import { MultiSelect } from "react-multi-select-component";
 import DateRangePicker from "@wojtekmaj/react-daterange-picker";
+import { format } from "date-fns";
+
 import DataTable from "./components/dataTable";
 import KeyMetrics from "./components/keyMetrics";
 import MainLayout from "./components/layouts/mainLayout";
 import Modal from "./components/layouts/modal";
 import AddRecordForm from "./components/forms/addRecordForm";
-import axios from "axios";
 import { recordTableColumns } from "./data/recordTableColumns";
-import { useMemo } from "react";
 import { metricsCalculator } from "@/lib/metricsCalculator";
 
 export default function Home() {
@@ -32,8 +33,15 @@ export default function Home() {
   const [propertyList, setPropertyList] = useState([]);
   const columns = useMemo(() => recordTableColumns, []);
 
-  const fetchRecords = async () => {
-    const response = await axios.get("/api/record");
+  const fetchRecords = async (searchParams) => {
+    
+    let searchParamsString = "";
+
+    if (searchParams) {
+      searchParamsString = `?${searchParams}`;
+    }
+
+    const response = await axios.get(`/api/record${searchParamsString}`);
     setRecords(response.data.response);
     const [income, expense, net] = metricsCalculator(response.data.response);
     setMetrics({
@@ -44,6 +52,7 @@ export default function Home() {
   };
 
   const fetchProperties = async () => {
+    
     const options = [];
     const response = await axios.get("api/property");
 
@@ -64,9 +73,37 @@ export default function Home() {
 
   useEffect(() => {
     if (addMode === false) {
-      fetchRecords();
+      onFilterHandler();
     }
   }, [addMode]);
+
+  const onFilterHandler = () => {
+    let searchParams = [];
+
+    if (selected && selected.length > 0) {
+      let tempArray = [];
+      selected.forEach((property) => {
+        tempArray.push(property.value);
+      });
+      searchParams.push("property=" + tempArray.join("_"));
+    }
+
+    if (dateRange[0]) {
+      console.log(format(new Date(dateRange[0]), "yyyy-MM-dd"))
+      searchParams.push(
+        `startdate=${String(format(new Date(dateRange[0]), "yyyy-MM-dd"))}`
+      );
+    }
+
+    if (dateRange[1]) {
+      console.log(format(new Date(dateRange[1]), "yyyy-MM-dd"))
+      searchParams.push(
+        `enddate=${String(format(new Date(dateRange[1]), "yyyy-MM-dd"))}`
+      );
+    }
+
+    fetchRecords(searchParams.join("&"));
+  };
 
   return (
     <MainLayout currentPage="activities">
@@ -86,11 +123,17 @@ export default function Home() {
             labelledBy="Select"
           />
           <DateRangePicker
-            className=" bg-white text-black"
-            calendarClassName="text-black rounded-lg"
+            className="h-[40px] bg-white text-gray-800"
+            calendarClassName="text-gray-800 rounded-lg"
             onChange={setDateRange}
             value={dateRange}
           />
+          <button
+            className="px-4 py-2 rounded-lg border-2 border-gray-500"
+            onClick={onFilterHandler}
+          >
+            Filter
+          </button>
         </div>
         <button
           className="px-4 py-2 text-sm font-bold rounded-lg bg-gradient-to-br from-green-400 to-blue-600"
@@ -108,7 +151,7 @@ export default function Home() {
       <div className="grow flex flex-col overflow-scroll">
         <div className="flex mb-4 items-center">
           <h4>All Records</h4>
-          <h4 className="ml-4 text-gray-700">Recurring</h4>
+          {/* <h4 className="ml-4 text-gray-700">Recurring</h4> */}
         </div>
         {records && <DataTable data={records} columns={columns} />}
       </div>
