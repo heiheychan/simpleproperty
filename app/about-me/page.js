997075fff5d19b4a50567eee29ec7041eb17a35a
Bill Chan <heiheychan@MacBook-Pro.local>
2023-08-06 12:44:18 -0400

    Escaped letter

--- app/about-me/page.js
+++ app/about-me/page.js
@@ -7,7 +7,7 @@ export default function page() {
       <main className="w-full flex flex-col justify-center items-center pt-48">
         <h4 className="mb-4">About me</h4>
         <p className="text-center">
-          Hi, I'm Bill, a Product Manager based in New York. Love
+          Hi, I&apos;m Bill, a Product Manager based in New York. Love
           coding when I am not product managing.{" "}
           <p className="underline text-blue-500 mt-4"><a href="https://www.linkedin.com/in/billcch/" target="_blank">
             Connect with me on Linkedin
